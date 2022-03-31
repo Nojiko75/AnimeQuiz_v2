2f59db16a2a6a721bbef7e4bedaa5bef845c0ef4
Nojiko75 <mkpi.julie@gmail.com>
2022-03-31 14:30:51 +0200

    Add game menu

--- app/src/main/java/com/nojiko/tanoshi/animequiz_v2/SplashScreenActivity.kt
+++ app/src/main/java/com/nojiko/tanoshi/animequiz_v2/SplashScreenActivity.kt
@@ -10,7 +10,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        startActivity(Intent(this@SplashScreenActivity, GameActivity::class.java))
         finish()
     }
 }
