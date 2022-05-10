667c45ee05705e57aeb094211928eee67e789677
Nojiko75 <mkpi.julie@gmail.com>
2022-05-10 11:21:07 +0200

    Remove splash screen and Main activity

--- app/src/main/java/com/nojiko/tanoshi/animequiz_v2/SplashFragment.kt
+++ app/src/main/java/com/nojiko/tanoshi/animequiz_v2/SplashFragment.kt
@@ -1,6 +1,8 @@
 package com.nojiko.tanoshi.animequiz_v2
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,12 +15,10 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Handler(Looper.myLooper()!!).postDelayed({
+            val action = SplashFragmentDirections.actionSplashFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }, 1000)
         return inflater.inflate(R.layout.fragment_splash, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val action = SplashFragmentDirections.actionSplashFragmentToHomeFragment()
-        findNavController().navigate(action)
     }
 }
