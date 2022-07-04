113c8c08e41cd7ba8308a71634910e0740e95c63
Julie MKPI <mkpi.julie@gmail.com>
2022-07-04 18:10:23 +0200

    Add coming soon message for features not yet developed

--- app/src/main/java/com/nojiko/tanoshi/animequiz_v2/DoneDialogFragment.kt
+++ app/src/main/java/com/nojiko/tanoshi/animequiz_v2/DoneDialogFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -31,6 +32,13 @@ class DoneDialogFragment : DialogFragment() {
         val action = DoneDialogFragmentDirections.actionDoneDialogFragmentToHomeFragment()
         binding.homeButton.setOnClickListener {
             findNavController().navigate(action)
+        }
+        binding.rankingButton.setOnClickListener {
+            Toast.makeText(
+                context,
+                getString(R.string.coming_soon),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
--- app/src/main/java/com/nojiko/tanoshi/animequiz_v2/MenuCharacterFragment.kt
+++ app/src/main/java/com/nojiko/tanoshi/animequiz_v2/MenuCharacterFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.NavHostFragment
@@ -19,6 +20,12 @@ class MenuCharacterFragment : Fragment() {
 
         val action = HomeFragmentDirections.actionHomeFragmentToCharacterFragment()
         binding.easyButton.setOnClickListener { navigateFromHome(action) }
+        binding.mediumButton.setOnClickListener {
+            Toast.makeText(context, getString(R.string.coming_soon), Toast.LENGTH_SHORT).show()
+        }
+        binding.hardButton.setOnClickListener {
+            Toast.makeText(context, getString(R.string.coming_soon), Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateView(
