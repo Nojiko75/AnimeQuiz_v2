5a5e73e4ac4290a89e9959665017647cd46e5c58
Julie MKPI <mkpi.julie@gmail.com>
2022-07-04 15:36:21 +0200

    Change status bar color for Android 12

--- app/src/main/java/com/nojiko/tanoshi/animequiz_v2/GameActivity.kt
+++ app/src/main/java/com/nojiko/tanoshi/animequiz_v2/GameActivity.kt
@@ -1,7 +1,9 @@
 package com.nojiko.tanoshi.animequiz_v2
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class GameActivity : AppCompatActivity() {
 
@@ -9,6 +11,9 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Hide action bar for Android 12
         supportActionBar?.hide()
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(applicationContext, R.color.teal_700)
+
         setContentView(R.layout.activity_game)
     }
 }
